f018499a10c16a29d1d4db6ac89129cd1e923af1
swagatika <swagatikam1964@gmail.com>
2021-08-07 12:02:57 +0530

    Med-stock

---
+++ src/main/java/com/cognizant/medicinestock/controller/MedicineStockController.java
@@ -0,0 +1,141 @@
+package com.cognizant.medicinestock.controller;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.medicinestock.exception.TokenValidationFailedException;
+import com.cognizant.medicinestock.feignclient.AuthenticationFeignClient;
+import com.cognizant.medicinestock.model.JwtResponse;
+import com.cognizant.medicinestock.model.MedicineStock;
+import com.cognizant.medicinestock.service.MedicineStockServiceImpl;
+
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class MedicineStockController {
+
+
+	@Autowired
+	private AuthenticationFeignClient authFeignClient;
+
+	@Autowired
+	private MedicineStockServiceImpl medicineStockService;
+
+	@GetMapping("/medicine-stock-information")
+	public ResponseEntity<?> getMedicineStockInformation(@RequestHeader(name = "Authorization") String token) {
+		log.info("START");
+		log.info("TOKEN {}:", token);
+		List<MedicineStock> medicineStockInformation = null;
+		try {
+			JwtResponse jwtResponse = authFeignClient.verifyToken(token);
+			log.info("JwtResponse {}:", jwtResponse);
+			if (jwtResponse.isValid()) {
+				log.info("TOKEN IS VALID");
+				medicineStockInformation = medicineStockService.getMedicineStockInformation();
+			}
+		} catch (FeignException e) {
+			log.error("TOKEN VALIDATION FAILED");
+			throw new TokenValidationFailedException("TOKEN EXPIRED");
+		}
+		log.info("END");
+		return new ResponseEntity<>(medicineStockInformation, HttpStatus.OK);
+
+	}
+
+	@PostMapping("/byTreatingAilment/{treatingAilment}")
+	public ResponseEntity<?> getMedicineByTreatingAilment(@RequestHeader(name = "Authorization") String token,
+			@PathVariable("treatingAilment") String treatingAilment) {
+		List<String> medicines = new ArrayList<>();
+		
+		List<MedicineStock> medicineByTargetAilment = medicineStockService
+				.getMedicineByTargetAilment(treatingAilment);
+		
+		JwtResponse jwtResponse = authFeignClient.verifyToken(token);
+		
+		boolean x=medicineByTargetAilment.isEmpty();
+		
+		log.info("START");
+		log.info("TOKEN {}:", token);
+		log.info("TREATING AILMENT {}:", treatingAilment);
+		try {
+			log.info("JWT RESPONSE {}:", jwtResponse);
+			if (jwtResponse.isValid()) {
+				log.info("TOKEN IS VALID");
+				log.info("MEDICINE BY TARGET AILMENT {}: ", medicineByTargetAilment);
+				if(x==true)
+				{
+					System.out.println("List is empty");
+				}
+				else
+				{
+					for (Iterator iterator = medicineByTargetAilment.iterator(); iterator.hasNext();) {
+						MedicineStock medicineStock = (MedicineStock) iterator.next();
+						medicines.add(medicineStock.getName());
+					}
+				}
+			}
+		} catch (FeignException e) {
+			log.error("TOKEN VALIDATION FAILED");
+			throw new TokenValidationFailedException("TOKEN EXPIRED");
+		}
+		log.info("END");
+		return new ResponseEntity<>(medicines, HttpStatus.OK);
+
+	}
+
+
+	@PostMapping("/get-stock-count/{medicine}")
+	public ResponseEntity<?> getStockCountForMedicine(@RequestHeader(name = "Authorization") String token,
+			@PathVariable("medicine") String medicine) {
+		log.info("START");
+		
+		JwtResponse jwtResponse = authFeignClient.verifyToken(token);
+
+		MedicineStock medicineStock = null;
+		try {
+			if (jwtResponse.isValid()) {
+				medicineStock = medicineStockService.getNumberOfTabletsInStockByName(medicine);
+			}
+		} catch (FeignException e) {
+			log.info("EXCEPTION: TOKEN EXPIRED");
+			throw new TokenValidationFailedException("TOKEN EXPIRED");
+		}
+		log.info("END");
+		return new ResponseEntity<>(medicineStock, HttpStatus.OK);
+	}
+
+	
+	@PostMapping("/update-stock/{medicine}/{count}")
+	public Boolean updateNumberOfTabletsInStockByName(@RequestHeader(name = "Authorization") String token,
+			@PathVariable("medicine") String medicine, @PathVariable("count") int count) {
+
+		log.info("START");
+		Boolean ans = false;
+		JwtResponse jwtResponse = authFeignClient.verifyToken(token);
+
+		try {
+			
+			if (jwtResponse.isValid()) {
+				ans = medicineStockService.updateNumberOfTabletsInStockByName(medicine, count);
+			}
+		} catch (FeignException e) {
+			log.info("EXCEPTION : TOKEN EXPIRED");
+			throw new TokenValidationFailedException("TOKEN EXPIRED");
+		}
+		log.info("END");
+		return ans;
+
+	}
+}
